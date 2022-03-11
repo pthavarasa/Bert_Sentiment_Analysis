c10e24433c01100f73d08c4b5f82ee5a80950e24
AndyGuillaume <andy.guillaume75@gmail.com>
2022-03-11 18:34:31 +0100

    set frontend in js

---
+++ Client/index.js
@@ -0,0 +1,44 @@
+const getInputContent = () => inputBtn.value;
+const existClass = (el, classEl) => [...el.classList].includes(classEl)
+const setActiveBtn = (el, otherEl) => {
+    error.innerHTML = ""
+    if (emotion !== el.value) {
+        emotion = el.value;
+        if (!existClass(el.target, el.class)) el.target.classList.toggle(el.class)
+        if (existClass(otherEl.target, otherEl.class)) otherEl.target.classList.toggle(otherEl.class)
+    }
+}
+const setupLog = (el, otherEl, value) => {
+    otherEl.innerHTML = "";
+    el.innerHTML = value;
+}
+
+const submitBtn = document.getElementById('submit');
+const goodBtn = document.getElementById('good');
+const badBtn = document.getElementById('bad');
+const inputBtn = document.getElementById('input');
+const error = document.getElementById('error');
+const succes = document.getElementById('succes');
+const good = { target: goodBtn, class: 'good-selected', value: 1 }
+const bad = { target: badBtn, class: 'bad-selected', value: 0 }
+let emotion = -1;
+
+goodBtn.addEventListener('click', () => {setActiveBtn(good, bad)})
+badBtn.addEventListener('click', () => {setActiveBtn(bad, good)})
+
+submitBtn.addEventListener('click', () => {
+    setupLog(error, succes, "")
+    if (emotion > -1 && getInputContent()) {
+        emotion = -1;
+        if (existClass(good.target, good.class)) good.target.classList.remove(good.class)
+        else if (existClass(bad.target, bad.class)) bad.target.classList.remove(bad.class)
+        fetch(`/?content=${getInputContent()}&emotion=${emotion}`).then(response => {
+            setupLog(succes, error, "The result")
+        }).catch(err => {
+            setupLog(error, succes, "Error during communication with server")
+        })
+    } else {
+        if (emotion < 0) setupLog(error, succes, "You must specify emotion")
+        else if (!getInputContent()) setupLog(error, succes, "You must specify sentence")
+    }
+})
